41ef7c8ed0ac1540d7761a04d9d8156bdd5ac1f8
AlexiWolf <alexi@wolvesin.space>
2024-03-05 06:41:25 -0600

    Moved the `generic_event_queue` code into `wolf_engine_events`
    Mostly to make maintaining it easier.  The code in `generic_event_queue` was
    originally written as part of Wolf Engine, but I was moved out to allow it to
    be shared with other crates.  The issue is it makes maintaining it pretty
    annoying.
    
    I'm also not sure the reasons for moving it out of Wolf Engine's crate are
    even valid anymore, especially considering the modules are now intended to be
    used independently.  I may just completely discontinue the
    `generic_event_queue` package in favor of just using `wolf_engine_events`
    directly.  Still not 100% on that yet, tho, so I'll probably consider it for a
    while before actually doing anything about it.

---
+++ crates/wolf_engine_events/src/event_queue.rs
@@ -0,0 +1,33 @@
+/// The event-receiving half of an Event-Queue.
+pub trait EventReceiver<E> {
+    /// Returns the next event in the queue.
+    fn next_event(&mut self) -> Option<E>;
+}
+
+/// The event-sending half of an Event-Queue.
+///
+/// Event Senders can be freely, and safely cloned, given away, and even sent across threads.
+pub trait EventSender<E>: Send + Sync {
+    /// Sends an event to the receiver.  
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the receiver has been dropped.
+    fn send_event(&self, event: E) -> Result<(), ReceiverDroppedError>;
+}
+
+/// An error indicating the [`EventReceiver`] associated with an [`EventSender`] has been dropped,
+/// and there is nowhere to send events.
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub struct ReceiverDroppedError;
+
+impl std::error::Error for ReceiverDroppedError {}
+
+impl std::fmt::Display for ReceiverDroppedError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Failed to send the event, because the receiver was dropped."
+        )
+    }
+}
