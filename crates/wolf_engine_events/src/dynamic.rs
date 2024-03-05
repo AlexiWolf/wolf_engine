e3d0d403dc3c869200ed681fde9ec20a1701556f
AlexiWolf <alexi@wolvesin.space>
2024-03-05 02:13:45 -0600

    Added docs to the `DynamicEventSender` trait

--- crates/wolf_engine_events/src/dynamic.rs
+++ crates/wolf_engine_events/src/dynamic.rs
@@ -12,6 +12,8 @@ pub use wolf_engine_codegen::Event;
 /// Represents a [`Boxed`](Box) dynamic [`Event`].
 pub type EventBox = Box<dyn Event>;
 
+/// An [`EventSender`](crate::EventSender) helper which takes a dynamic [`Event`], and
+/// automatically [`Boxes`](Box) it for the caller.
 pub trait DynamicEventSender {
     fn send_event<T: Event + 'static>(&self, event: T) -> Result<(), ReceiverDroppedError>;
 }
