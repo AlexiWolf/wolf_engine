0a30e7b01ec1890b7b67dc6f8e52e5e6e6bba696
AlexiWolf <alexi@wolvesin.space>
2024-03-05 05:30:54 -0600

    Added docs to the event types

--- crates/wolf_engine_events/src/dynamic/event_loop.rs
+++ crates/wolf_engine_events/src/dynamic/event_loop.rs
@@ -9,9 +9,11 @@ use events::*;
 pub mod events {
     use crate::dynamic::DynamicEvent;
 
+    /// Indicates the engine has quit.
     #[derive(DynamicEvent, Debug)]
     pub struct Quit;
-
+    
+    /// Indicates all other events have been cleared, and a new frame / tick is about to begin.
     #[derive(DynamicEvent, Debug)]
     pub struct EventsCleared;
 }
