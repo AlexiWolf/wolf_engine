15857b44e4a21bc8a4b37c494fe0b312ada0793d
AlexiWolf <alexi@wolvesin.space>
2024-03-11 13:27:17 -0500

    Add test cases of keyboard Pressed / Relased conversions

--- crates/wolf_engine_input/src/lib.rs
+++ crates/wolf_engine_input/src/lib.rs
@@ -4,5 +4,11 @@
 pub mod winit {
     #[cfg(test)]
     mod winit_conversion_tests {
+        use test_case::test_case;
+        #[test_case(KeyCode::KeyA, ElementState::Pressed => Some(Input::KeyDown(Key { scancode: 0, keycode: None })))]
+        #[test_case(KeyCode::KeyA, ElementState::Released=> Some(Input::KeyUp(Key { scancode: 0, keycode: None })))]
+        fn should_convert_key_events(key_code: KeyCode, state: ElementState) -> Option<Input> {
+            None
+        }
     }
 }
