44ba401db35db3237ad3a0969c6611a0e16bbe3a
AlexiWolf <alexi@wolvesin.space>
2024-03-11 14:28:09 -0500

    Add alpha-numeric keys to `KeyCode` enum

--- crates/wolf_engine_input/src/lib.rs
+++ crates/wolf_engine_input/src/lib.rs
@@ -11,7 +11,44 @@ pub struct Key {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum KeyCode {}
+pub enum KeyCode {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+    Key0,
+    Key1,
+    Key2,
+    Key3,
+    Key4,
+    Key5,
+    Key6,
+    Key7,
+    Key8,
+    Key9,
+}
 
 #[cfg(feature = "winit")]
 pub mod winit {
