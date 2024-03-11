fdb8b07c66ba5171127e41d26689e9e0004d3cf4
AlexiWolf <alexi@wolvesin.space>
2024-03-11 13:36:00 -0500

    Add test of `winit_to_input` fn

--- crates/wolf_engine_input/src/lib.rs
+++ crates/wolf_engine_input/src/lib.rs
@@ -17,11 +17,29 @@ pub enum KeyCode {}
 pub mod winit {
     #[cfg(test)]
     mod winit_conversion_tests {
+        use ::winit::{
+            event::{DeviceEvent, DeviceId, ElementState, Event, RawKeyEvent},
+            keyboard::{KeyCode, PhysicalKey},
+        };
         use test_case::test_case;
+
+        use super::*;
+        use crate::*;
+
         #[test_case(KeyCode::KeyA, ElementState::Pressed => Some(Input::KeyDown(Key { scancode: 0, keycode: None })))]
         #[test_case(KeyCode::KeyA, ElementState::Released=> Some(Input::KeyUp(Key { scancode: 0, keycode: None })))]
         fn should_convert_key_events(key_code: KeyCode, state: ElementState) -> Option<Input> {
-            None
+            let var_name = RawKeyEvent {
+                physical_key: PhysicalKey::Code(key_code),
+                state,
+            };
+            let event = Event::DeviceEvent::<()> {
+                // SAFETY: Don't pass this to any winit functions.
+                device_id: unsafe { DeviceId::dummy() },
+                event: DeviceEvent::Key(var_name),
+            };
+
+            winit_to_input(event)
         }
     }
 }
