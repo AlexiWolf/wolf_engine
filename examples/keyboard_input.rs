53933f7a9233ebe9bd9e4e2385352f6eb85dc2a9
AlexiWolf <alexi@wolvesin.space>
2024-03-12 23:35:16 -0500

    Add `keyboard_input` example
    Mostly to define the expected API.
    
    I don't think it's possible to unit-test with Winit's WindowEvent keyboard inputs, since
    they have private fields.  I guess this will have to do well-enough.

---
+++ examples/keyboard_input.rs
@@ -0,0 +1,29 @@
+use winit::{
+    dpi::PhysicalSize,
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+use wolf_engine::input::InputConversion;
+
+pub fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let _window = WindowBuilder::new()
+        .with_title("Wolf Engine - Keyboard Input Example")
+        .with_inner_size(PhysicalSize::new(800, 600))
+        .with_resizable(false)
+        .build(&event_loop)
+        .unwrap();
+    let _ = event_loop.run(|event, window_target| match &event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => window_target.exit(),
+        Event::WindowEvent { .. } => {
+            if let Some(input) = event.to_input() {
+                println!("{:?}", input);
+            }
+        }
+        _ => (),
+    });
+}
