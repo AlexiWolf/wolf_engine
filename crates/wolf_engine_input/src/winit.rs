5c72111e488d19451886a2bd4eb004a3691fe529
AlexiWolf <alexi@wolvesin.space>
2024-03-12 23:41:47 -0500

    Add `InputConversion` impl for `WindowEvent`

--- crates/wolf_engine_input/src/winit.rs
+++ crates/wolf_engine_input/src/winit.rs
@@ -1,6 +1,7 @@
 use crate::keyboard::{Key, KeyCode};
 use crate::{Input, InputConversion};
 
+use winit::event::{KeyEvent, WindowEvent};
 use winit::{
     event::{DeviceEvent, ElementState, Event, RawKeyEvent},
     keyboard::{KeyCode as WinitKeyCode, PhysicalKey},
@@ -14,12 +15,31 @@ impl<T> InputConversion for winit::event::Event<T> {
                 event: DeviceEvent::Key(key_event),
                 ..
             } => Some(key_event.clone().into()),
+            Event::WindowEvent { event, .. } => event.to_input(),
+            _ => None,
+        }
+    }
+}
+
+impl InputConversion for WindowEvent {
+    fn to_input(&self) -> Option<Input> {
+        match self {
+            WindowEvent::KeyboardInput { event, .. } => Some(event.clone().into()),
             _ => None,
         }
     }
 }
 
 impl Into<Input> for RawKeyEvent {
+    fn into(self) -> Input {
+        match self.state {
+            ElementState::Pressed => Input::KeyDown(self.physical_key.into()),
+            ElementState::Released => Input::KeyUp(self.physical_key.into()),
+        }
+    }
+}
+
+impl Into<Input> for KeyEvent {
     fn into(self) -> Input {
         match self.state {
             ElementState::Pressed => Input::KeyDown(self.physical_key.into()),
@@ -156,65 +176,6 @@ impl Into<KeyCode> for WinitKeyCode {
             WinitKeyCode::NumpadDecimal => KeyCode::NumpadDecimal,
 
             _ => KeyCode::Unknown,
-        }
-    }
-}
-
-#[cfg(test)]
-mod winit_conversion_tests {
-    use ::winit::{
-        event::{DeviceEvent, DeviceId, ElementState, Event, RawKeyEvent},
-        keyboard::{KeyCode as WinitKeyCode, PhysicalKey},
-    };
-    use test_case::test_case;
-
-    use super::*;
-    use crate::*;
-
-    #[test_case(WinitKeyCode::KeyA, ElementState::Pressed, Some(KeyCode::A))]
-    #[test_case(WinitKeyCode::KeyA, ElementState::Released, Some(KeyCode::A))]
-    fn should_convert_key_events(
-        key_code: WinitKeyCode,
-        state: ElementState,
-        expected_keycode: Option<KeyCode>,
-    ) {
-        let event = create_winit_event(key_code, state);
-        let input = event.to_input().expect("Input was not converted");
-        match input {
-            Input::KeyDown(key) => {
-                assert!(
-                    state.is_pressed(),
-                    "The key was not pressed, so this should be a KeyUp event"
-                );
-                assert_ne!(key.scancode, 0, "The converted scancode should not be 0");
-                assert_eq!(
-                    key.keycode, expected_keycode,
-                    "The keycode did not match what was expected"
-                );
-            }
-            Input::KeyUp(key) => {
-                assert!(
-                    !state.is_pressed(),
-                    "The key was pressed, so this should be a KeyDown event"
-                );
-                assert_ne!(key.scancode, 0, "The converted scancode should not be 0");
-                assert_eq!(
-                    key.keycode, expected_keycode,
-                    "The keycode did not match what was expected"
-                );
-            }
-        }
-    }
-
-    fn create_winit_event(key_code: WinitKeyCode, state: ElementState) -> winit::Event<()> {
-        let raw_key_event = RawKeyEvent {
-            physical_key: PhysicalKey::Code(key_code),
-            state,
-        };
-        Event::DeviceEvent::<()> {
-            // SAFETY: Don't pass this to any winit functions.
-            device_id: unsafe { DeviceId::dummy() },
-            event: DeviceEvent::Key(raw_key_event),
         }
     }
 }
