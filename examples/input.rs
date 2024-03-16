495955cd36f77e722b7735086ac15f8279ab539d
AlexiWolf <alexi@wolvesin.space>
2024-03-15 19:19:32 -0500

    Change input example to create window on `Resume`

--- examples/input.rs
+++ examples/input.rs
@@ -9,17 +9,22 @@ use wolf_engine_input::Input;
 
 pub fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let _window = WindowBuilder::new()
-        .with_title("Wolf Engine - Input Example")
-        .with_inner_size(PhysicalSize::new(800, 600))
-        .with_resizable(false)
-        .build(&event_loop)
-        .unwrap();
+    let mut window = None;
     let _ = event_loop.run(|event, window_target| {
         if let Some(input) = event.to_input() {
             process_input(&input);
         }
         match event {
+            Event::Resumed => {
+                window = Some(
+                    WindowBuilder::new()
+                        .with_title("Wolf Engine - Input Example")
+                        .with_inner_size(PhysicalSize::new(800, 600))
+                        .with_resizable(false)
+                        .build(&window_target)
+                        .unwrap(),
+                );
+            }
             Event::AboutToWait => {
                 window_target.listen_device_events(winit::event_loop::DeviceEvents::Always)
             }
