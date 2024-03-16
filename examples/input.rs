598bbf81ba3a4d03ef84104115671c564f9c305c
AlexiWolf <alexi@wolvesin.space>
2024-03-15 19:09:21 -0500

    Update input example's window title

--- examples/input.rs
+++ examples/input.rs
@@ -10,7 +10,7 @@ use wolf_engine_input::Input;
 pub fn main() {
     let event_loop = EventLoop::new().unwrap();
     let _window = WindowBuilder::new()
-        .with_title("Wolf Engine - Keyboard Input Example")
+        .with_title("Wolf Engine - Input Example")
         .with_inner_size(PhysicalSize::new(800, 600))
         .with_resizable(false)
         .build(&event_loop)
