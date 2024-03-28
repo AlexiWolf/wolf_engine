ac8b67602fad0d2e719b846fffa19e2d2d1f3e75
AlexiWolf <alexi@wolvesin.space>
2024-03-28 12:17:56 -0500

    Add re-export of `wolf_engine_graphics`
    As `graphics`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #[cfg(feature = "events")]
 pub use wolf_engine_events as events;
 
+#[cfg(feature = "graphics")]
+pub use wolf_engine_graphics as graphics;
+
 #[cfg(feature = "input")]
 pub use wolf_engine_input as input;
