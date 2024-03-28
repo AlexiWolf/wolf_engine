ab6c5823b0b9ff7fb5566888c7c8624ed44cbafb
AlexiWolf <alexi@wolvesin.space>
2024-03-28 11:48:36 -0500

    Add window setup, and expected graphics init code

--- examples/graphics.rs
+++ examples/graphics.rs
@@ -1,1 +1,42 @@
-pub fn main() {}
+use winit::{
+    dpi::PhysicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{EventLoop, EventLoopWindowTarget},
+    window::{Window, WindowBuilder},
+};
+use wolf_engine_graphics::GraphicsContext;
+
+struct Context {
+    window: Option<Window>,
+    graphics: Option<GraphicsContext>,
+}
+
+pub fn main() {
+    let mut context = Context {
+        window: None,
+        graphics: None,
+    };
+    let event_loop = EventLoop::new().unwrap();
+    let _ = event_loop.run(|event, window_target| handle_event(&mut context, event, window_target));
+}
+
+fn handle_event(context: &mut Context, event: Event<()>, event_loop: &EventLoopWindowTarget<()>) {
+    match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => event_loop.exit(),
+        Event::Resumed => {
+            context.window = Some(
+                WindowBuilder::new()
+                    .with_title("Wolf Engine - Graphics Example")
+                    .with_inner_size(PhysicalSize::new(800, 600))
+                    .with_resizable(false)
+                    .build(event_loop)
+                    .unwrap(),
+            );
+            context.graphics = Some(wolf_engine_graphics::init().unwrap());
+        }
+        _ => (),
+    }
+}
