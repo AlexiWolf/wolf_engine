bbc3933807d2ad6ec1e5fd46c3c14ed6f003ce0e
AlexiWolf <alexi@wolvesin.space>
2024-03-29 19:20:58 -0500

    Change expected use of `graphics::init()`

--- crates/wolf_engine_graphics/src/lib.rs
+++ crates/wolf_engine_graphics/src/lib.rs
@@ -3,3 +3,11 @@ pub async fn init() -> Result<GraphicsContext, &'static str> {
 }
 
 pub struct GraphicsContext {}
+
+#[cfg(test)]
+mod graphics_init_tests {
+    #[test]
+    fn should_use_builder_pattern() {
+        let graphics_context = crate::init().build().unwrap();
+    }
+}
