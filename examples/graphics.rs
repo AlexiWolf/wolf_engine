a7e77c3c2b494abf47dddaa119f7ce57b0aaa488
AlexiWolf <alexi@wolvesin.space>
2024-03-31 11:59:35 -0500

    Change `window` to immutable

--- examples/graphics.rs
+++ examples/graphics.rs
@@ -6,7 +6,7 @@ use winit::{
 };
 pub fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let mut window = WindowBuilder::new()
+    let window = WindowBuilder::new()
         .with_title("Wolf Engine - Graphics Example")
         .with_inner_size(PhysicalSize::new(800, 600))
         .with_resizable(false)
