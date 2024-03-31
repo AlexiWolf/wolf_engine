9b601daa343b4d292e443d067d0530957f792e09
AlexiWolf <alexi@wolvesin.space>
2024-03-31 11:59:09 -0500

    Fix borrowing issues

--- examples/graphics.rs
+++ examples/graphics.rs
@@ -1,50 +1,33 @@
 use winit::{
     dpi::PhysicalSize,
     event::{Event, WindowEvent},
-    event_loop::{EventLoop, EventLoopWindowTarget},
-    window::{Window, WindowBuilder},
+    event_loop::EventLoop,
+    window::WindowBuilder,
 };
-use wolf_engine_graphics::GraphicsContext;
-
-struct Context {
-    window: Option<Window>,
-    graphics: Option<GraphicsContext>,
-}
-
 pub fn main() {
-    let mut context = Context {
-        window: None,
-        graphics: None,
-    };
     let event_loop = EventLoop::new().unwrap();
-    let _ = event_loop.run(|event, window_target| handle_event(&mut context, event, window_target));
-}
-
-fn handle_event(context: &mut Context, event: Event<()>, event_loop: &EventLoopWindowTarget<()>) {
-    match event {
+    let mut window = WindowBuilder::new()
+        .with_title("Wolf Engine - Graphics Example")
+        .with_inner_size(PhysicalSize::new(800, 600))
+        .with_resizable(false)
+        .build(&event_loop)
+        .expect("Failed to create the window");
+    let mut graphics = None;
+    let _ = event_loop.run(|event, window_target| match event {
         Event::WindowEvent {
             event: WindowEvent::CloseRequested,
             ..
-        } => event_loop.exit(),
+        } => window_target.exit(),
         Event::Resumed => {
-            context.window = Some(
-                WindowBuilder::new()
-                    .with_title("Wolf Engine - Graphics Example")
-                    .with_inner_size(PhysicalSize::new(800, 600))
-                    .with_resizable(false)
-                    .build(event_loop)
-                    .expect("Failed to create the window"),
-            );
-            let window = context.window.as_ref().unwrap();
             let window_size = window.inner_size();
             let (width, height) = (window_size.width, window_size.height);
-            context.graphics = Some(
+            graphics = Some(
                 pollster::block_on(
-                    wolf_engine::graphics::init().build(Some((window, (width, height)))),
+                    wolf_engine::graphics::init().build(Some((&window, (width, height)))),
                 )
                 .expect("Failed to create the graphics context"),
             );
         }
         _ => (),
-    }
+    });
 }
