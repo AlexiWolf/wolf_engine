dfb292c0a5c62e61bbd6f1081ac241686b158105
AlexiWolf <alexi@wolvesin.space>
2024-05-22 16:51:30 -0500

    Add stub of `WindowSettings`

--- crates/wolf_engine_window/src/lib.rs
+++ crates/wolf_engine_window/src/lib.rs
@@ -1,5 +1,13 @@
 pub type WindowSystem = ((), ());
 
+pub struct WindowSettings {}
+
+impl Default for WindowSettings {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
 pub fn init() -> WindowSystem {
     ((), ())
 }
