47372fbfeaa620274c309c83d24c7ceb41741853
AlexiWolf <alexi@wolvesin.space>
2024-05-22 16:51:10 -0500

    Add missing import

--- examples/window.rs
+++ examples/window.rs
@@ -1,3 +1,5 @@
+use wolf_engine::window::WindowSettings;
+
 pub fn main() {
     let (event_loop, context) = wolf_engine::window::init();
     let window = context.create_window(WindowSettings::default());
