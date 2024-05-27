c0588dfe8585ecb2b2abc8dd37aca9371865e37b
AlexiWolf <alexi@wolvesin.space>
2024-05-27 18:24:48 -0500

    Add stub of match statement to example

--- examples/window.rs
+++ examples/window.rs
@@ -2,12 +2,14 @@ use wolf_engine::prelude::*;
 use wolf_engine::window::WindowSettings;
 
 pub fn main() {
-    let (event_queue, context) = wolf_engine::window::init();
+    let (mut event_queue, context) = wolf_engine::window::init();
     let window = context.create_window(WindowSettings::default());
 
     loop {
         while let Some(event) = event_queue.next_event() {
-
+            match event {
+                _ => (),
+            }
         }
     }
 }
