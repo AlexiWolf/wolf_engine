2d87c41dd17cdde718db9c37583d5fa98e32b070
AlexiWolf <alexi@wolvesin.space>
2024-05-27 18:23:18 -0500

    Add `prelude` module with export of `EventReceiver`

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,8 @@ pub use wolf_engine_input as input;
 
 #[cfg(feature = "window")]
 pub use wolf_engine_window as window;
+
+pub mod prelude {
+    #[cfg(feature = "events")]
+    pub use crate::events::EventReceiver;
+}
