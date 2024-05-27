8d7fa785c3a69bbb7c1fb7efa4d9bc4379bde802
AlexiWolf <alexi@wolvesin.space>
2024-05-27 18:16:40 -0500

    Add use of window event queue

--- examples/window.rs
+++ examples/window.rs
@@ -1,6 +1,12 @@
 use wolf_engine::window::WindowSettings;
 
 pub fn main() {
-    let (event_loop, context) = wolf_engine::window::init();
+    let (event_queue, context) = wolf_engine::window::init();
     let window = context.create_window(WindowSettings::default());
+
+    loop {
+        while let Some(event) = event_queue.next_event() {
+
+        }
+    }
 }
