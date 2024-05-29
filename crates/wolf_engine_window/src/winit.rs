cd4ef9e515be8ffaeac1e0d43833c33710c5f577
AlexiWolf <alexi@wolvesin.space>
2024-05-29 15:58:02 -0500

    Add setup for `WinitAdapter`

--- crates/wolf_engine_window/src/winit.rs
+++ crates/wolf_engine_window/src/winit.rs
@@ -7,7 +7,8 @@ pub struct WinitBackend;
 
 impl WindowBackend for WinitBackend {
     fn init(self, event_sender: MpscEventSender<WindowEvent>) -> Box<dyn WindowBackendAdapter> {
-        let event_loop = ::winit::event_loop::EventLoop::new().unwrap();
-        let winit_adapter = WinitAdapter::new(event_loop);
+        let event_loop = EventLoop::new().unwrap();
+        let winit_adapter = WinitAdapter::new(event_sender, event_loop);
+        Box::from(winit_adapter)
     }
 }
