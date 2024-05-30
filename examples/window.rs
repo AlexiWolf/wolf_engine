5821b2b98fa7eae80684829949b96fed826c7658
AlexiWolf <alexi@wolvesin.space>
2024-05-30 01:34:13 -0500

    Add close handling to example

--- examples/window.rs
+++ examples/window.rs
@@ -1,13 +1,15 @@
 use wolf_engine::prelude::*;
 use wolf_engine::window::WindowSettings;
+use wolf_engine_window::WindowEvent;
 
 pub fn main() {
     let (mut event_queue, context) = wolf_engine::window::init().unwrap();
     let window = context.create_window(WindowSettings::default());
 
-    loop {
+    'main: loop {
         while let Some(event) = event_queue.next_event() {
             match event {
+                WindowEvent::CloseRequested { .. } => break 'main,
                 _ => (),
             }
         }
