c9ade71810486c93b633b4d6e87c704cad5ce033
AlexiWolf <alexi@wolvesin.space>
2024-06-04 19:23:48 -0500

    Add basic setup for `Pixels` library to example

--- examples/window.rs
+++ examples/window.rs
@@ -1,11 +1,17 @@
+use pixels::{Pixels, SurfaceTexture};
 use wolf_engine::prelude::*;
 use wolf_engine::window::WindowSettings;
 use wolf_engine_window::WindowEvent;
 
 pub fn main() {
     let (mut event_queue, context) = wolf_engine::window::init().unwrap();
+
     let window = context.create_window(WindowSettings::default());
-    let (width, height) = window.size();
+    let mut pixels = {
+        let (width, height) = window.size();
+        let surface_texture = SurfaceTexture::new(width, height, &window);
+        Pixels::new(width, height, surface_texture).unwrap();
+    };
 
     'main: loop {
         while let Some(event) = event_queue.next_event() {
