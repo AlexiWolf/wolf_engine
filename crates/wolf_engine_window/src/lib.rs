0eb346a1164c008da0dd10775d6fde6c94ec3b8b
AlexiWolf <alexi@wolvesin.space>
2024-06-04 23:11:05 -0500

    Add rwh traits, and auto-impls

--- crates/wolf_engine_window/src/lib.rs
+++ crates/wolf_engine_window/src/lib.rs
@@ -1,10 +1,5 @@
 use std::sync::Arc;
 
-pub use raw_window_handle;
-
-use raw_window_handle::{
-    HasDisplayHandle, HasRawDisplayHandle, HasRawWindowHandle, HasWindowHandle,
-};
 use uuid::Uuid;
 use winit::WinitBackend;
 use wolf_engine_events::{
@@ -75,9 +70,30 @@ impl Window {
     }
 }
 
-pub trait WindowTrait:
-    HasWindowHandle + HasDisplayHandle + HasRawWindowHandle + HasRawDisplayHandle
-{
+#[cfg(feature = "rwh_06")]
+pub trait HasRwh6Handles: rwh_06::HasWindowHandle + rwh_06::HasDisplayHandle {}
+#[cfg(feature = "rwh_06")]
+impl<T> HasRwh6Handles for T where T: rwh_06::HasWindowHandle + rwh_06::HasDisplayHandle {}
+
+#[cfg(not(feature = "rwh_06"))]
+pub trait HasRwh6Handles {}
+#[cfg(not(feature = "rwh_06"))]
+impl<T> HasRwh6Handles for T {}
+
+#[cfg(feature = "rwh_05")]
+pub trait HasRwh5Handles: rwh_05::HasRawWindowHandle + rwh_05::HasRawDisplayHandle {}
+#[cfg(feature = "rwh_05")]
+impl<T> HasRwh5Handles for T where T: rwh_05::HasRawWindowHandle + rwh_05::HasRawDisplayHandle {}
+
+#[cfg(not(feature = "rwh_05"))]
+pub trait HasRwh5Handles {}
+#[cfg(not(feature = "rwh_05"))]
+impl<T> HasRwh5Handles for T {}
+
+pub trait HasRawWindowHandles: HasRwh6Handles + HasRwh5Handles {}
+impl<T> HasRawWindowHandles for T where T: HasRwh6Handles + HasRwh5Handles {}
+
+pub trait WindowTrait: HasRawWindowHandles {
     fn title(&self) -> String;
     fn size(&self) -> (u32, u32);
 }
