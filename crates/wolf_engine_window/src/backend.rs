2ac0b3f58eccb043e53880ac8140d6b73889cc32
AlexiWolf <alexi@wolvesin.space>
2024-06-11 16:38:42 -0500

    Move `WindowTrait` to `backend.rs`
    I think it makes a bit more sense here, since the trait is only used to
    interact with windows created by the back-end.

--- crates/wolf_engine_window/src/backend.rs
+++ crates/wolf_engine_window/src/backend.rs
@@ -1,6 +1,16 @@
 use wolf_engine_events::mpsc::MpscEventSender;
 
-use crate::{event::WindowEvent, settings::WindowSettings, Window};
+use crate::{
+    error::WindowError, event::WindowEvent, raw_window_handle::HasRawWindowHandles,
+    settings::WindowSettings, Window,
+};
+
+pub trait WindowTrait: HasRawWindowHandles + Send + Sync {
+    fn title(&self) -> Result<String, WindowError>;
+    fn size(&self) -> Result<(u32, u32), WindowError>;
+    fn is_open(&self) -> bool;
+    fn close(&self);
+}
 
 pub trait WindowBackend {
     fn init(self, event_sender: MpscEventSender<WindowEvent>) -> Box<dyn WindowBackendAdapter>;
