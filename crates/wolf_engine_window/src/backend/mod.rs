3726b7b5e7ad67668bb78d77875ae16ffba2a16e
AlexiWolf <alexi@wolvesin.space>
2024-06-11 23:44:42 -0500

    Move `backend` to its own folder

---
+++ crates/wolf_engine_window/src/backend/mod.rs
@@ -0,0 +1,17 @@
+pub mod winit;
+
+use crate::{
+    error::WindowError, raw_window_handle::HasRawWindowHandles, settings::WindowSettings, Window,
+};
+
+pub trait WindowTrait: HasRawWindowHandles + Send + Sync {
+    fn title(&self) -> Result<String, WindowError>;
+    fn size(&self) -> Result<(u32, u32), WindowError>;
+    fn is_open(&self) -> bool;
+    fn close(&self);
+}
+
+pub trait WindowBackendAdapter {
+    fn pump_events(&self);
+    fn create_window(&self, settings: WindowSettings) -> Window;
+}
