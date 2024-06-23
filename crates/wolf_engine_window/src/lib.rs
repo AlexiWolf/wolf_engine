53d41013d35ff8182159ef065b3f0098c9e4aa4b
AlexiWolf <alexi@wolvesin.space>
2024-06-23 18:20:44 -0500

    Add stub of `WindowContextBuilder`, and methods
    `with_title()`, `with_size()`, and `resizable()` methods are included.

--- crates/wolf_engine_window/src/lib.rs
+++ crates/wolf_engine_window/src/lib.rs
@@ -2,4 +2,23 @@ pub fn init() -> WindowContextBuilder {
     todo!()
 }
 
+pub struct WindowContextBuilder {}
+
+impl WindowContextBuilder {
+    pub fn with_title(mut self, title: &str) -> Self {
+        self
+    }
+
+    pub fn with_size(mut self, size: (u32, u32)) -> Self {
+        self
+    }
+
+    pub fn resizable(mut self, is_resizable: bool) -> Self {
+        self
+    }
+
+    pub fn build(self) -> WindowContext {
+        todo!()
+    }
+}
 
