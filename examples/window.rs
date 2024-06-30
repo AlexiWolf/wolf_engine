a2b7e610dd700fd7bce38bb2eec0f7b006588307
AlexiWolf <alexi@wolvesin.space>
2024-06-30 17:26:02 -0500

    Update expected usage of `WindowContext`

--- examples/window.rs
+++ examples/window.rs
@@ -2,25 +2,22 @@ use pixels::{wgpu::Color, Pixels, SurfaceTexture};
 use wolf_engine::window::WindowEvent;
 
 fn main() {
-    let window_context = wolf_engine::window::init()
+    let window = wolf_engine::window::init()
         .with_title("Wolf Engine - Window Example")
         .with_size((800, 600))
         .with_resizable(false)
         .build();
 
-    let mut pixels = None;
+    let mut pixels = {
+        let (width, height) = window.size();
+        let surface_texture = SurfaceTexture::new(width, height, &window);
+        Pixels::new(width, height, surface_texture).unwrap()
+    };
+    pixels.clear_color(Color::RED);
 
-    window_context.run(|event, window| match event {
-        WindowEvent::Resume => {
-            pixels = Some({
-                let (width, height) = window.size();
-                let surface_texture = SurfaceTexture::new(width, height, &window);
-                Pixels::new(width, height, surface_texture).unwrap()
-            });
-            pixels.as_mut().unwrap().clear_color(Color::RED);
-            println!("Hello, world!");
-        }
-        WindowEvent::Render => pixels.as_ref().unwrap().render().unwrap(),
+    window.run(|event, _window| match event {
+        WindowEvent::Resume => println!("Hello, world!"),
+        WindowEvent::Render => pixels.render().unwrap(),
         WindowEvent::Closed => println!("Goodbye, World!"),
         _ => (),
     });
