dc9f7d9cb43aea23fb27d04feb2dd4b079d86dab
AlexiWolf <alexi@wolvesin.space>
2024-07-24 00:38:33 -0500

    Add re-export of `wolf_engine_framework` crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 pub use wolf_engine_events as events;
 
+#[cfg(feature = "framework")]
+pub use wolf_engine_framework as framework;
+
 #[cfg(feature = "input")]
 pub use wolf_engine_input as input;
 
