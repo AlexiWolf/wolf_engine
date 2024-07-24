e3705dbf2f5783d0c244111337b377e6745791c9
AlexiWolf <alexi@wolvesin.space>
2024-07-24 00:55:26 -0500

    Add stub of `Engine` / `EngineBuilder`

--- crates/wolf_engine_framework/src/lib.rs
+++ crates/wolf_engine_framework/src/lib.rs
@@ -1,3 +1,19 @@
+pub struct Engine;
+
+impl Engine {
+    pub fn run<G: Game>(self, game: G) {
+        todo!()
+    }
+}
+
+pub struct EngineBuilder;
+
+impl EngineBuilder {
+    pub fn build(self) -> Result<Engine, ()> {
+        todo!()
+    }
+}
+
 pub trait Game {
     fn setup(&mut self, context: &mut Context) {}
 }
