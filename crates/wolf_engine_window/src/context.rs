bbb3f6af7179fe39c08cd5ccb72bf0d44470329d
AlexiWolf <alexi@wolvesin.space>
2024-08-11 22:45:05 -0500

    Add impl for `Borderless` full-screen

--- crates/wolf_engine_window/src/context.rs
+++ crates/wolf_engine_window/src/context.rs
@@ -1,9 +1,12 @@
 use anyhow::anyhow;
-use winit::event_loop::ActiveEventLoop;
+use winit::{
+    event_loop::ActiveEventLoop,
+    window::{Fullscreen, WindowAttributes},
+};
 
 use crate::{
     error::{OsError, WindowError},
-    Window, WindowIdMap, WindowSettings,
+    FullscreenMode, Window, WindowIdMap, WindowSettings,
 };
 
 /// A link to the window system.
@@ -22,7 +25,18 @@ impl<'event_loop> WindowContext<'event_loop> {
 
     /// Create a new [`Window`].
     pub fn create_window(&self, window_settings: WindowSettings) -> Result<Window, WindowError> {
-        match self.event_loop.create_window(window_settings.into()) {
+        let fullscreen_mode = window_settings.fullscreen_mode.clone();
+        let mut window_attributes: WindowAttributes = window_settings.into();
+        if let Some(fullscreen_mode) = fullscreen_mode {
+            let monitor_handle = self.event_loop.primary_monitor();
+            match fullscreen_mode {
+                FullscreenMode::Borderless => {
+                    window_attributes = window_attributes
+                        .with_fullscreen(Some(Fullscreen::Borderless(monitor_handle)))
+                }
+            }
+        }
+        match self.event_loop.create_window(window_attributes) {
             Ok(winit_window) => {
                 let window = Window::new(winit_window, self.window_ids.id_remover());
                 self.window_ids.insert(&window);
