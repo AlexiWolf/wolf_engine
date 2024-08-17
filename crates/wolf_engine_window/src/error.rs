625b3bb471a7a2d81b4661bb05bbe084f6212bfa
AlexiWolf <alexi@wolvesin.space>
2024-08-17 17:48:19 -0500

    Update / add required trait impls for error types

--- crates/wolf_engine_window/src/error.rs
+++ crates/wolf_engine_window/src/error.rs
@@ -1,5 +1,7 @@
+use std::sync::Arc;
+
 /// A general-purpose error for the Window system.
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, PartialEq, Clone)]
 pub enum WindowError {
     #[error("Operation is unsupported by the OS")]
     OsError(#[from] OsError),
@@ -8,17 +10,43 @@ pub enum WindowError {
 }
 
 /// Indicates an operation is unsupported by the OS.
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, Clone)]
 #[error(transparent)]
 pub struct OsError {
-    #[from]
-    error: anyhow::Error,
+    error: Arc<anyhow::Error>,
+}
+
+impl From<anyhow::Error> for OsError {
+    fn from(error: anyhow::Error) -> Self {
+        Self {
+            error: Arc::new(error),
+        }
+    }
+}
+
+impl PartialEq for OsError {
+    fn eq(&self, other: &Self) -> bool {
+        self.error.to_string() == other.error.to_string()
+    }
 }
 
 /// Indicates an operation is unsupported by specific back-end system.
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, Clone)]
 #[error(transparent)]
 pub struct UnsupportedError {
-    #[from]
-    error: anyhow::Error,
+    error: Arc<anyhow::Error>,
+}
+
+impl From<anyhow::Error> for UnsupportedError {
+    fn from(error: anyhow::Error) -> Self {
+        Self {
+            error: Arc::new(error),
+        }
+    }
+}
+
+impl PartialEq for UnsupportedError {
+    fn eq(&self, other: &Self) -> bool {
+        self.error.to_string() == other.error.to_string()
+    }
 }
