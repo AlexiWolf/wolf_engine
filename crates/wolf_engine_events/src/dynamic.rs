0c93585db9613f692c98f29d74183c2e687ec2bf
AlexiWolf <alexi@wolvesin.space>
2024-09-05 00:38:28 -0500

    Remove `EventLoop` struct

--- crates/wolf_engine_events/src/dynamic.rs
+++ crates/wolf_engine_events/src/dynamic.rs
@@ -1,8 +1,3 @@
-use crate::{
-    mpsc::{MpscEventReceiver, MpscEventSender},
-    EventReceiver,
-};
-
 use downcast_rs::*;
 
 use std::fmt::Debug;
@@ -11,25 +6,3 @@ pub type AnyEvent = Box<dyn Event>;
 
 pub trait Event: Downcast + Debug + 'static {}
 impl_downcast!(Event);
-
-pub struct EventLoop {
-    sender: MpscEventSender<AnyEvent>,
-    receiver: MpscEventReceiver<AnyEvent>,
-}
-
-impl EventLoop {
-    pub fn new() -> Self {
-        let (sender, receiver) = crate::mpsc::event_queue();
-        Self { sender, receiver }
-    }
-
-    pub fn event_sender(&self) -> MpscEventSender<AnyEvent> {
-        self.sender.to_owned()
-    }
-}
-
-impl EventReceiver<AnyEvent> for EventLoop {
-    fn next_event(&mut self) -> Option<AnyEvent> {
-        self.receiver.next_event()
-    }
-}
