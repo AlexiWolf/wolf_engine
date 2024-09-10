353883b0cd5d396f6277007b5f0b64426ea9b6b9
AlexiWolf <alexi@wolvesin.space>
2024-09-10 05:59:38 -0500

    Change window system usage in example

--- examples/window.rs
+++ examples/window.rs
@@ -1,21 +1,24 @@
 use pixels::{wgpu::Color, Pixels, SurfaceTexture};
-use wolf_engine::window::event::Event;
-use wolf_engine_window::{event::WindowEvent, Window, WindowSettings};
+use wolf_engine_events::{dynamic::AnyEvent, mpsc};
+use wolf_engine_window::{
+    event::{Event, WindowEvent},
+    WindowSettings,
+};
 
 fn main() {
-    let (event_loop, context) = wolf_engine::window::init().build().unwrap();
+    let (event_sender, event_reciever) = mpsc::event_queue::<AnyEvent>();
+    let window_context = wolf_engine::window::init(event_sender.clone()).unwrap();
 
     let mut canvas = None;
-    let mut window: Option<Window> = None;
+    let window = window_context.create_window(
+        WindowSettings::default()
+            .with_title("Wolf Engine - Window Example")
+            .with_size((800, 600)),
+    );
 
-    event_loop.run(|event| match event {
+    wolf_engine_winit::run(event_reciever, |event| match event {
         Event::Started => {
             println!("Hello, world!");
-            context.create_window(
-                WindowSettings::default()
-                    .with_title("Wolf Engine - Window Example")
-                    .with_size((800, 600)),
-            );
         }
         Event::EventsCleared => {
             if let Some(window) = window.as_ref() {
