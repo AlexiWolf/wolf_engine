6a7bb3c661274f61fc1a9d957cab8ff72e526740
AlexiWolf <alexi@wolvesin.space>
2024-09-19 20:29:49 -0500

    Add stub of winit main loop

--- crates/wolf_engine_winit/src/lib.rs
+++ crates/wolf_engine_winit/src/lib.rs
@@ -1,3 +1,9 @@
+use winit::{
+    application::ApplicationHandler,
+    event::StartCause,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::WindowId,
+};
 use wolf_engine_events::{dynamic::AnyEvent, mpsc::MpscEventReceiver};
 use wolf_engine_window::{error::WindowError, event::Event};
 
@@ -5,5 +11,30 @@ pub fn run<E: FnMut(Event)>(
     event_receiver: MpscEventReceiver<AnyEvent>,
     event_handler: E,
 ) -> Result<(), WindowError> {
+    let event_loop = EventLoop::new().unwrap();
+    let mut winit_adapter = WinitAdapter::new();
+    let _ = event_loop.run_app(&mut winit_adapter);
     Ok(())
+}
+
+struct WinitAdapter {}
+
+impl WinitAdapter {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl ApplicationHandler for WinitAdapter {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {}
+
+    fn new_events(&mut self, event_loop: &ActiveEventLoop, cause: StartCause) {}
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+    }
 }
