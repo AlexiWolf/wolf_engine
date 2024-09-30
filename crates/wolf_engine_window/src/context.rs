498208c8f7e9aef5bb3558b620cc9f6e8704fd94
AlexiWolf <alexi@wolvesin.space>
2024-09-29 20:19:17 -0500

    Store windows in the context object
    I think I can make this work with full `Window` objects.  If not, I can
    probably store `WindowState` objects instead.
    
    My goal here is for the `WindowContext` to do all, if not most, of it's
    own bookkeeping, because that's better than having to keep multiple
    handles / state objects in sync externally.

--- crates/wolf_engine_window/src/context.rs
+++ crates/wolf_engine_window/src/context.rs
@@ -12,14 +12,14 @@ use crate::{event::BackendEvent, raw_window_handle::WindowHandle, Window, Window
 /// A link to the window system.
 pub struct WindowContext {
     event_sender: MpscEventSender<AnyEvent>,
-    window_handles: Arc<RwLock<HashMap<Uuid, WindowHandle>>>,
+    windows: Arc<RwLock<HashMap<Uuid, Window>>>,
 }
 
 impl WindowContext {
     pub(crate) fn new(event_sender: MpscEventSender<AnyEvent>) -> Self {
         Self {
             event_sender,
-            window_handles: Arc::new(RwLock::new(HashMap::new())),
+            windows: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
@@ -36,18 +36,18 @@ impl WindowContext {
     }
 
     pub fn window_handle(&self, window: &Window) -> Option<WindowHandle> {
-        match self.window_handles.read().unwrap().get(&window.id()) {
+        match self.windows.read().unwrap().get(&window.id()) {
             Some(handle) => Some(handle.to_owned()),
             None => None,
         }
     }
 
     pub fn insert_window_handle(&self, uuid: Uuid, handle: WindowHandle) {
-        self.window_handles.write().unwrap().insert(uuid, handle);
+        self.windows.write().unwrap().insert(uuid, handle);
     }
 
     pub fn remove_window_handle(&self, uuid: Uuid) {
-        self.window_handles.write().unwrap().remove(&uuid);
+        self.windows.write().unwrap().remove(&uuid);
     }
 
     /// Stops the event loop.
