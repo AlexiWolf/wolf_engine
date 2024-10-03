476f82f92853e1bfd1eec488eb44d22304d2c9e3
AlexiWolf <alexi@wolvesin.space>
2024-10-02 23:14:51 -0500

    Add `InitError`

--- crates/wolf_engine_window/src/error.rs
+++ crates/wolf_engine_window/src/error.rs
@@ -1,6 +1,8 @@
 /// A general-purpose error for the Window system.
 #[derive(thiserror::Error, Debug, PartialEq, Clone)]
 pub enum WindowError {
+    #[error("Failed to initialize the window system")]
+    InitError(String),
     #[error("Operation is unsupported by the window system.")]
     UnsupportedError(String),
 }
