d702ef9a17429b7bafa6d9d75e233c5ac22d80db
AlexiWolf <alexi@wolvesin.space>
2024-10-18 01:06:35 -0500

    Impl `init()` for winit backend

--- crates/wolf_engine_winit/src/lib.rs
+++ crates/wolf_engine_winit/src/lib.rs
@@ -1,11 +1,30 @@
-use wolf_engine_events::{dynamic::AnyEvent, event_loop::EventLoop};
-use wolf_engine_window::{backend::WindowSystem, error::WindowError};
+use winit::event_loop::EventLoop;
+use wolf_engine_events::{
+    dynamic::AnyEvent,
+    mpsc::{self, MpscEventReceiver, MpscEventSender},
+};
+use wolf_engine_window::{backend::WindowSystem, error::WindowError, WindowContext};
 
 pub fn init() -> Result<WinitBackend, WindowError> {
-    todo!()
+    let (event_sender, event_receiver) = mpsc::event_queue();
+    let window_context = WindowContext::new(event_sender.clone());
+    match EventLoop::new() {
+        Ok(event_loop) => Ok(WinitBackend {
+            event_sender,
+            event_receiver,
+            window_context,
+            event_loop,
+        }),
+        Err(error) => Err(WindowError::InitError(error.to_string())),
+    }
 }
 
-pub struct WinitBackend {}
+pub struct WinitBackend {
+    event_sender: MpscEventSender<AnyEvent>,
+    event_receiver: MpscEventReceiver<AnyEvent>,
+    window_context: WindowContext,
+    event_loop: EventLoop<()>,
+}
 
 impl WindowSystem for WinitBackend {
     fn context(&self) -> wolf_engine_window::WindowContext {
@@ -13,7 +32,7 @@ impl WindowSystem for WinitBackend {
     }
 }
 
-impl EventLoop<AnyEvent> for WinitBackend {
+impl wolf_engine_events::event_loop::EventLoop<AnyEvent> for WinitBackend {
     fn event_sender(&self) -> wolf_engine_events::mpsc::MpscEventSender<AnyEvent> {
         todo!()
     }
