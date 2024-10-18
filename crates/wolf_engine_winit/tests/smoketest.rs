22da1f5e0ba6d845e9c98d0f9b301bbed0d1f1da
AlexiWolf <alexi@wolvesin.space>
2024-10-17 21:57:43 -0500

    Update test / examples to new design

--- crates/wolf_engine_winit/tests/smoketest.rs
+++ crates/wolf_engine_winit/tests/smoketest.rs
@@ -1,32 +1,38 @@
 use std::process::ExitCode;
 
+use event::WindowEvent;
 use libtest_mimic::{Arguments, Failed, Trial};
-use wolf_engine_window::event::WindowEvent;
-use wolf_engine_window::*;
+use wolf_engine_events::event_loop::EventLoop;
+use wolf_engine_window::{backend::WindowSystem, *};
 
 pub fn main() -> ExitCode {
     let mut args = Arguments::from_args();
     args.test_threads = Some(1);
-    let tests = vec![Trial::test("should_run_and_quit", test)];
+    let tests = vec![Trial::test("winit_smoketest", test)];
     libtest_mimic::run(&args, tests).exit_code()
 }
 
 fn test() -> Result<(), Failed> {
-    let window_system = wolf_engine_winit::init().build().unwrap();
+    let window_system = wolf_engine_winit::init().unwrap();
     let context = window_system.context();
 
     let mut has_quit = false;
 
-    window_system.run(|event| match event {
-        WindowEvent::Started => {
-            let _window = context.create_window(WindowSettings::default().with_visible(false));
+    window_system.run(|event| {
+        if let Some(window_event) = event.downcast_ref::<WindowEvent>() {
+            match window_event {
+                WindowEvent::Started => {
+                    let _window =
+                        context.create_window(WindowSettings::default().with_visible(false));
+                }
+                WindowEvent::WindowCreated(_, window_result) => {
+                    window_result.as_ref().expect("Window creation succeeded");
+                    context.exit();
+                }
+                WindowEvent::Exited => *&mut has_quit = true,
+                _ => (),
+            }
         }
-        WindowEvent::WindowEvent(_, WindowEvent::Created(window_result)) => {
-            let _window = window_result.expect("Window creation succeeded");
-            context.exit();
-        }
-        WindowEvent::Exited => *&mut has_quit = true,
-        _ => (),
     });
     Ok(())
 }
--- examples/window.rs
+++ examples/window.rs
@@ -1,55 +1,58 @@
 use pixels::{wgpu::Color, Pixels, SurfaceTexture};
-use wolf_engine::window::event::Event;
-use wolf_engine_window::{event::WindowEvent, Window, WindowSettings};
+use wolf_engine_events::event_loop::EventLoop;
+use wolf_engine_window::{backend::WindowSystem, event::WindowEvent, WindowSettings};
 
 fn main() {
-    let (event_loop, context) = wolf_engine::window::init().build().unwrap();
+    let window_system = wolf_engine_winit::init().unwrap();
+    let context = window_system.context();
 
     let mut canvas = None;
-    let mut window: Option<Window> = None;
+    let mut window = context.create_window(
+        WindowSettings::default()
+            .with_title("Wolf Engine - Window Example")
+            .with_size((800, 600)),
+    );
 
-    event_loop.run(|event| match event {
-        Event::Started => {
-            println!("Hello, world!");
-            context.create_window(
-                WindowSettings::default()
-                    .with_title("Wolf Engine - Window Example")
-                    .with_size((800, 600)),
-            );
-        }
-        Event::EventsCleared => {
-            if let Some(window) = window.as_ref() {
-                window.redraw();
-            }
-        }
-        Event::WindowEvent(_window_id, event) => match event {
-            WindowEvent::Created(window_result) => {
-                window = Some(window_result.expect("Window creation succeeded"));
-                canvas = Some({
-                    let window = window.as_ref().unwrap();
-                    let (width, height) = window.size();
-                    let surface_texture = SurfaceTexture::new(width, height, window);
-                    let mut pixels = Pixels::new(width, height, surface_texture).unwrap();
-                    pixels.clear_color(Color::RED);
-                    pixels
-                });
-            }
-            WindowEvent::RedrawRequested => {
-                if let Some(pixels) = &canvas {
-                    pixels.render().unwrap();
+    window_system.run(|event| {
+        if let Some(window_event) = event.downcast_ref::<WindowEvent>() {
+            match window_event {
+                WindowEvent::Started => {
+                    println!("Hello, world!");
+                    context.create_window(
+                        WindowSettings::default()
+                            .with_title("Wolf Engine - Window Example")
+                            .with_size((800, 600)),
+                    );
                 }
-            }
-            WindowEvent::Resized(width, height) => {
-                if let Some(pixels) = &mut canvas {
-                    pixels.resize_buffer(width, height).unwrap();
-                    pixels.resize_surface(width, height).unwrap();
+                WindowEvent::EventsCleared => {
+                    window.redraw();
                 }
+                WindowEvent::WindowCreated(_, _) => {
+                    canvas = Some({
+                        let handle = window.handle().unwrap();
+                        let (width, height) = window.size();
+                        let surface_texture = SurfaceTexture::new(width, height, &handle);
+                        let mut pixels = Pixels::new(width, height, surface_texture).unwrap();
+                        pixels.clear_color(Color::RED);
+                        pixels
+                    });
+                }
+                WindowEvent::WindowRedrawRequested(_) => {
+                    if let Some(pixels) = &canvas {
+                        pixels.render().unwrap();
+                    }
+                }
+                WindowEvent::WindowResized(_, width, height) => {
+                    if let Some(pixels) = &mut canvas {
+                        pixels.resize_buffer(*width, *height).unwrap();
+                        pixels.resize_surface(*width, *height).unwrap();
+                    }
+                }
+                WindowEvent::Input(_, input) => println!("Input into window: {:?}", input),
+                WindowEvent::WindowClosed(_) => context.exit(),
+                WindowEvent::Exited => println!("Goodbye, World!"),
+                _ => (),
             }
-            WindowEvent::Input(input) => println!("Input into window: {:?}", input),
-            WindowEvent::Closed => context.exit(),
-            _ => (),
-        },
-        Event::Exited => println!("Goodbye, World!"),
-        _ => (),
+        }
     });
 }
