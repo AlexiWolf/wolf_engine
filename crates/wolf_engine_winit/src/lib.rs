905049bd4be84d0e5b31c9a34a43356ed420437f
AlexiWolf <alexi@wolvesin.space>
2024-10-21 21:28:48 -0500

    Add impl for `WinitBackend::run()` method

--- crates/wolf_engine_winit/src/lib.rs
+++ crates/wolf_engine_winit/src/lib.rs
@@ -7,16 +7,19 @@ use wolf_engine_events::{
     dynamic::AnyEvent,
     mpsc::{self, MpscEventReceiver, MpscEventSender},
 };
-use wolf_engine_window::{backend::WindowSystem, error::WindowError, WindowContext};
+use wolf_engine_window::{
+    backend::WindowSystem, error::WindowError, WindowContext, WindowContextEventSender,
+};
 
 pub fn init() -> Result<WinitBackend, WindowError> {
     let (event_sender, event_receiver) = mpsc::event_queue();
-    let (window_context, window_event_sender) = WindowContext::new(event_sender.clone());
+    let (window_context, window_context_event_sender) = WindowContext::new(event_sender.clone());
     match EventLoop::new() {
         Ok(event_loop) => Ok(WinitBackend {
             event_sender,
             event_receiver,
             window_context,
+            window_context_event_sender,
             event_loop,
         }),
         Err(error) => Err(WindowError::InitError(error.to_string())),
@@ -27,29 +30,39 @@ pub struct WinitBackend {
     event_sender: MpscEventSender<AnyEvent>,
     event_receiver: MpscEventReceiver<AnyEvent>,
     window_context: WindowContext,
+    window_context_event_sender: WindowContextEventSender,
     event_loop: EventLoop<()>,
 }
 
 impl WindowSystem for WinitBackend {
-    fn context(&self) -> wolf_engine_window::WindowContext {
+    fn context(&self) -> WindowContext {
         self.window_context.clone()
     }
 }
 
 impl wolf_engine_events::event_loop::EventLoop<AnyEvent> for WinitBackend {
-    fn event_sender(&self) -> wolf_engine_events::mpsc::MpscEventSender<AnyEvent> {
-        todo!()
+    fn event_sender(&self) -> MpscEventSender<AnyEvent> {
+        self.event_sender.clone()
     }
 
-    fn run<F: FnMut(AnyEvent)>(self, event_handler: F) {}
+    fn run<F: FnMut(AnyEvent)>(self, event_handler: F) {
+        let mut winit_app = WinitApp {
+            event_sender: self.event_sender,
+            event_receiver: self.event_receiver,
+            window_context: self.window_context,
+            window_context_event_sender: self.window_context_event_sender,
+        };
+        let event_loop = self.event_loop;
+
+        let _ = event_loop.run_app(&mut winit_app);
+    }
 }
 
-struct WinitApp {}
-
-impl WinitApp {
-    pub(crate) fn new() -> Self {
-        Self {}
-    }
+struct WinitApp {
+    event_sender: MpscEventSender<AnyEvent>,
+    event_receiver: MpscEventReceiver<AnyEvent>,
+    window_context: WindowContext,
+    window_context_event_sender: WindowContextEventSender,
 }
 
 impl ApplicationHandler for WinitApp {
