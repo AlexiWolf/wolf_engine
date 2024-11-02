2d2cb6ef9c68917ae6a6a053a52fd42211b71440
AlexiWolf <alexi@wolvesin.space>
2024-11-02 17:52:41 -0500

    Add docs for erorr variants

--- crates/wolf_engine_window/src/error.rs
+++ crates/wolf_engine_window/src/error.rs
@@ -1,8 +1,10 @@
 /// A general-purpose error for the Window system.
 #[derive(thiserror::Error, Debug, PartialEq, Clone)]
 pub enum WindowError {
+    /// Indicates the window system could not be initialized.
     #[error("Failed to initialize the window system")]
     InitError(String),
+    /// Indicates the operation is not supposrted by either the window system, or the current OS.
     #[error("Operation is unsupported by either the window system, or the current OS.")]
     UnsupportedError(String),
 }
