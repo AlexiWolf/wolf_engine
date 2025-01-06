b16cbd26181a9f987623f06832d0c64dd34721e6
AlexiWolf <alexi@wolvesin.space>
2025-01-06 16:05:50 -0600

    Add "tv-static" to `window` example

--- examples/window.rs
+++ examples/window.rs
@@ -1,17 +1,19 @@
 use pixels::{wgpu::Color, Pixels, SurfaceTexture};
+use rand::Rng;
 use wolf_engine_events::event_loop::EventLoop;
-use wolf_engine_window::{backend::WindowSystem, WindowEvent, WindowSettings};
+use wolf_engine_window::{backend::WindowSystem, Window, WindowEvent, WindowSettings};
 
 fn main() {
+    let mut rng = rand::thread_rng();
     let window_system = wolf_engine_winit::init().unwrap();
     let context = window_system.context();
 
-    let mut canvas = None;
     let window = context.create_window(
         WindowSettings::default()
             .with_title("Wolf Engine - Window Example")
             .with_size((800, 600)),
     );
+    let mut canvas = Canvas::from(window);
 
     window_system.run(|event| {
         if let Some(window_event) = event.downcast_ref::<WindowEvent>() {
@@ -20,28 +22,27 @@ fn main() {
                     println!("Hello, world!");
                 }
                 WindowEvent::EventsCleared => {
-                    window.redraw();
+                    canvas.window().redraw();
                 }
                 WindowEvent::WindowReady(_, _) => {
-                    canvas = Some({
-                        let handle = window.handle().unwrap();
-                        let (width, height) = window.size();
-                        let surface_texture = SurfaceTexture::new(width, height, &handle);
-                        let mut pixels = Pixels::new(width, height, surface_texture).unwrap();
-                        pixels.clear_color(Color::RED);
-                        pixels
-                    });
+                    canvas.reconfigure();
+                    canvas.pixels_mut().unwrap().enable_vsync(false);
                 }
                 WindowEvent::WindowRedrawRequested(_) => {
-                    if let Some(pixels) = &canvas {
-                        pixels.render().unwrap();
+                    let (width, height) = canvas.window.size();
+                    for x in 0..width {
+                        for y in 0..height {
+                            if rng.gen() {
+                                canvas.draw_pixel(x, y, [0xff, 0xff, 0xff, 0xff]);
+                            } else {
+                                canvas.draw_pixel(x, y, [0x00, 0x00, 0x00, 0xff]);
+                            }
+                        }
                     }
+                    canvas.render();
                 }
-                WindowEvent::WindowResized(_, width, height) => {
-                    if let Some(pixels) = &mut canvas {
-                        pixels.resize_buffer(*width, *height).unwrap();
-                        pixels.resize_surface(*width, *height).unwrap();
-                    }
+                WindowEvent::WindowResized(_, _, _) => {
+                    canvas.reconfigure();
                 }
                 WindowEvent::Input(uuid, input) => {
                     println!("Input into window ({:?}): {:?}", uuid, input)
@@ -52,4 +53,92 @@ fn main() {
             }
         }
     });
+}
+
+pub struct Canvas {
+    pixels: Option<Pixels>,
+    window: Window,
+}
+
+impl From<Window> for Canvas {
+    fn from(window: Window) -> Self {
+        Self {
+            pixels: None,
+            window,
+        }
+    }
+}
+
+impl Canvas {
+    pub fn reconfigure(&mut self) {
+        if self.window.handle().is_none() {
+            return;
+        }
+        let (width, height) = self.window.size();
+        if self.pixels.is_none() {
+            let handle = self.window.handle().unwrap();
+            let surface_texture = SurfaceTexture::new(width, height, &handle);
+            let pixels = Pixels::new(width, height, surface_texture).unwrap();
+            self.pixels = Some(pixels);
+        }
+
+        self.pixels
+            .as_mut()
+            .unwrap()
+            .resize_surface(width, height)
+            .unwrap();
+        self.pixels
+            .as_mut()
+            .unwrap()
+            .resize_buffer(width, height)
+            .unwrap();
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    pub fn pixels(&self) -> Option<&Pixels> {
+        if let Some(pixels) = &self.pixels {
+            Some(pixels)
+        } else {
+            None
+        }
+    }
+
+    pub fn pixels_mut(&mut self) -> Option<&mut Pixels> {
+        if let Some(pixels) = &mut self.pixels {
+            Some(pixels)
+        } else {
+            None
+        }
+    }
+
+    pub fn draw_pixel(&mut self, x: u32, y: u32, color: [u8; 4]) {
+        if let Some(pixels) = &mut self.pixels {
+            let (width, _) = self.window.size();
+            let index = x as usize + y as usize * width as usize;
+            write_pixel(color, index, pixels.frame_mut());
+        }
+    }
+
+    pub fn render(&mut self) {
+        if let Some(pixels) = &mut self.pixels {
+            pixels.render().unwrap();
+        }
+    }
+}
+
+pub fn write_pixel(color: [u8; 4], index: usize, buffer: &mut [u8]) {
+    let pixel_index = index * 4;
+    if pixel_index > buffer.len() {
+        panic!(
+            "Index out of bounds.  Buffer size is {}, but index was {}",
+            buffer.len(),
+            pixel_index
+        )
+    }
+    for (channel_i, value) in color.iter().enumerate() {
+        buffer[pixel_index + channel_i] = *value;
+    }
 }
